de814fb80b44a0b59ea3bcdd44ec77eb8f20ab50
djam388 <djam388@hotmail.com>
2022-07-27 10:39:55 +0500

    Attempt #2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,9 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     public static void main(String[] args) {
-        divide(10);
+        divide(11);
     }
-    static void divide(int toBeDivided) {
+    static void divide(double toBeDivided) {
         System.out.println(toBeDivided / 5);
     }
 }
